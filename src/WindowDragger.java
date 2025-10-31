515898c0bdd48348b553593973889d59afc86ce6
K1Dev <67011212055@msu.ac.th>
2025-11-01 05:40:43 +0700

    Improve window dragging - Add 10px threshold to prevent accidental window movement

--- src/WindowDragger.java
+++ src/WindowDragger.java
@@ -6,6 +6,8 @@ import java.awt.event.MouseEvent;
 public class WindowDragger extends MouseAdapter {
     private Point initialClick;
     private JFrame frame;
+    private boolean isDragging = false;
+    private static final int DRAG_THRESHOLD = 10;
     
     public WindowDragger(JFrame frame) {
         this.frame = frame;
@@ -14,16 +16,31 @@ public class WindowDragger extends MouseAdapter {
     @Override
     public void mousePressed(MouseEvent e) {
         initialClick = e.getPoint();
+        isDragging = false;
+    }
+    
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        isDragging = false;
+        initialClick = null;
     }
     
     @Override
     public void mouseDragged(MouseEvent e) {
         if (initialClick != null) {
-            int thisX = frame.getLocation().x;
-            int thisY = frame.getLocation().y;
-            
             int xMoved = e.getX() - initialClick.x;
             int yMoved = e.getY() - initialClick.y;
+            
+            if (!isDragging) {
+                double distance = Math.sqrt(xMoved * xMoved + yMoved * yMoved);
+                if (distance < DRAG_THRESHOLD) {
+                    return;
+                }
+                isDragging = true;
+            }
+            
+            int thisX = frame.getLocation().x;
+            int thisY = frame.getLocation().y;
             
             int X = thisX + xMoved;
             int Y = thisY + yMoved;
